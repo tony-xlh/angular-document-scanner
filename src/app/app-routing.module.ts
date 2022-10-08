6424b5161af539602596184afbd559021485f89c
xulihang <xulihanghai@163.com>
2022-10-08 15:31:24 +0800

    rename welcome page to settings page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: '', pathMatch: 'full', redirectTo: '/scanner' },
+  { path: 'settings', loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule) },
   { path: 'scanner', loadChildren: () => import('./pages/scanner/scanner.module').then(m => m.ScannerModule) }
 ];
 
