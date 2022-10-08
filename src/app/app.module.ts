02832769f9b9201a1f8ac0cf5d0b13b44f3f0b47
xulihang <xulihanghai@163.com>
2022-10-08 14:04:09 +0800

    add document viewer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { DocumentViewerComponent } from './document-viewer/document-viewer.component';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentViewerComponent
   ],
   imports: [
     BrowserModule,
