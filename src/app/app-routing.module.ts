b6ae81964367651acb5473dc50cb0d5d9d354e00
xulihang <xulihanghai@163.com>
2022-10-08 16:19:28 +0800

    add dwt for test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/scanner' },
   { path: 'settings', loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule) },
   { path: 'scanner', loadChildren: () => import('./pages/scanner/scanner.module').then(m => m.ScannerModule) }
 ];
--- src/app/pages/scanner/scanner.component.ts
+++ src/app/pages/scanner/scanner.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Dynamsoft from "dwt";
+import { WebTwain } from 'dwt/dist/types/WebTwain';
 
 @Component({
   selector: 'app-scanner',
@@ -7,13 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScannerComponent implements OnInit {
   public count:number;
+  public containerID = "dwtcontrolContainer";
+  public DWObject:WebTwain|undefined;
   constructor() { 
     this.count = 0
+    this.initDWT();
   }
 
   ngOnInit(): void {
   }
+
+  initDWT(){
+    Dynamsoft.DWT.RegisterEvent('OnWebTwainReady', () => {
+      this.DWObject = Dynamsoft.DWT.GetWebTwain(this.containerID);
+    });
+    Dynamsoft.DWT.ResourcesPath = "/dwt-resources";
+    Dynamsoft.DWT.Containers = [{
+        WebTwainId: 'dwtObject',
+        ContainerId: this.containerID
+    }];
+
+    Dynamsoft.DWT.Load();
+  }
+
   onClick(){
     this.count++;
+    this.DWObject?.AcquireImage();
   }
 }
